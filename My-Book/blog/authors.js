e62c0e20f48513c7f929c9a3dbb2322b85e34f44
Ayaan Zeeshan <muhammad.ayaan.zeeshan123@gmail.com>
2025-12-11 21:15:02 +0500

    Add author Ayaan to authors.js

--- My-Book/blog/authors.js
+++ My-Book/blog/authors.js
@@ -1,5 +1,14 @@
 module.exports = {
-  yangshun: { name: 'Shun Yang', title: 'Docusaurus Creator', url: 'https://github.com/yangshun' },
-  slorber: { name: 'Slorber', title: 'Contributor', url: 'https://github.com/slorber' },
-  ayaan: { name: 'Ayaan Zeeshan', title: 'Author', url: 'https://github.com/muhammadayaan2276' },
+  yangshun: {
+    name: 'Yangshun Tay',
+    url: 'https://github.com/yangshun',
+  },
+  slorber: {
+    name: 'Simon Lorber',
+    url: 'https://github.com/slorber',
+  },
+  ayaan: {
+    name: 'Ayaan Zeeshan',
+    url: 'https://github.com/muhammadayaan2276',
+  },
 };
